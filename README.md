91467c18c1110eca61a330314e1186741764edfd
cabdiaziz <abdiaziiz1856@gmail.com>
2023-01-24 02:22:48 +0300

    re-structured readme file and correct userRoute importing

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
+# My-App 24-jan-2023
+
+# User Field Tasks.
+
 [x] write a db connection
 [x] upload to github repository.
+[x] implement clean architecture
+
 [x] write three routes for user api
 [x] design users model
-[] write three requests for user api and services, query services.
+
+[] write three express methods
+[] write three logic layer methods
+[] write the query layer methods.
+
 [] connect to firebase and enable auth service.
 [] write auth middleware
 
-# my-app
+LAST STAPES :
+-Validation for models
+-Validation for data that comes from frontend.
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import connectMongoDB from "./src/framework/config/db.js";
-import userRoute from "./src/components/users/routes.js";
+import { userRoute } from "./src/components/users/index.js";
 
 const app = express();
 dotenv.config();
@@ -13,11 +13,11 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
 
-//*all users routes.
+//all users routes.
 app.use("/api/v1/auth", userRoute);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log("app is running on http://localhost:5000");
+    console.log("This app is running on http://localhost:5000");
 });
