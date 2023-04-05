5c2df99f9db43382b1b5b559233ae9cdf4199384
cabdiaziz <abdiaziiz1856@gmail.com>
2023-04-05 07:38:11 +0300

    prepared a validation arrays for task api;

--- src/components/tasks/validations.js
+++ src/components/tasks/validations.js
@@ -1,2 +1,8 @@
 // import express-validator
 // export and create a validation 'const' array for CRUD functions.
+import { check } from "express-validator";
+
+export const createNewTask = [];
+export const getTask = [];
+export const updateTask = [];
+export const deleteTask = [];
