cdb2ac7f94c22dcdd74d920eaedb1e8311340671
cabdiaziz <abdiaziiz1856@gmail.com>
2023-04-05 06:29:26 +0300

    added a validation package and added some description.

--- src/components/tasks/controller.js
+++ src/components/tasks/controller.js
@@ -1,4 +1,4 @@
-//? this file is express layer only calls functions and wait responses.
+//* this file is express layer only calls functions and wait responses.
 import {
   newTaskService,
   getAllTasksService,
@@ -6,8 +6,14 @@ import {
 } from "./index.js";
 import { createNewTask, findAllTasks, findAndUpdateTask } from "./index.js";
 import apiErrorHandler from "../../framework/middleware/apiErrorHandler.js";
+import asyncHandler from "express-async-handler";
 
-export const createTask = async (req, res) => {
+//? body data need a validation using express-validator
+
+// @desc  crate a new task.
+// @api   POST /api/v1/tasks.
+// @access private.
+export const createTask = asyncHandler(async (req, res) => {
   try {
     const { description } = req.body;
 
@@ -21,13 +27,16 @@ export const createTask = async (req, res) => {
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
-};
-export const getTasks = async (req, res) => {
+});
+
+// @desc  return all tasks.
+// @api   GET /api/v1/tasks.
+// @access private.
+export const getTasks = asyncHandler(async (req, res) => {
   try {
     //pagination.
-    console.log("req.params :-> ", req.params);
-    const page = 1;
-    const limit = 5;
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 5;
     const skip = (page - 1) * limit;
     const pagination = { limit, skip }; //result : 5
 
@@ -38,8 +47,12 @@ export const getTasks = async (req, res) => {
   } catch (err) {
     return res.status(400).json({ message: err.message });
   }
-};
-export const taskCompletion = async (req, res) => {
+});
+
+// @desc  update task completion.
+// @api   PUT /api/v1/tasks.
+// @access private.
+export const taskCompletion = asyncHandler(async (req, res) => {
   try {
     const { _id } = req.params;
     const { isCompleted } = req.body;
@@ -55,7 +68,7 @@ export const taskCompletion = async (req, res) => {
     console.log("Err ==", err);
     res.status(500).json({ message: err.message });
   }
-};
+});
 
 export const getOneTask = async (req, res) => {};
 export const updateTask = async (req, res) => {};
---
+++ src/components/tasks/validations.js
@@ -0,0 +1,2 @@
+// import express-validator
+// export and create a validation 'const' array for CRUD functions.
